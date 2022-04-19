f6ef32cd6f166fb2c2c67cb88094568e7aa7912d
lucamerola <82286885+lucamerola@users.noreply.github.com>
2022-04-19 14:38:46 +0200

    Update valori_desiderati.m

--- valori_desiderati.m
+++ valori_desiderati.m
@@ -6,12 +6,12 @@ function valori_desiderati(sv, ts)
     Mf=((pi/2)-atan( (((-2*csi^2)+(4*csi^4 +1)^0.5)^0.5)/(2*csi) ));
     WtWn=(-2*csi^2+(4*csi^4+1)^0.5)^0.5;
     Wt=WtWn*Wn;
-    disp("smorzamento csi = "+csi);
-    disp("sovraelongazione % = "+S);
-    disp("ts * wn = "+TW);
-    disp("Wn = "+Wn);
-    disp("Margine di fase = "+Mf);
-    disp("Margine di fase in gradi = "+(Mf*180/pi));
-    disp("Wt/Wn = "+WtWn);
-    disp("Wt = "+Wt);
+    disp("ξ = "+csi);
+    disp("S% = "+S);
+    disp("ts * ωn = "+TW);
+    disp("ωn = "+Wn);
+    disp("Mf(rad) = "+Mf);
+    disp("Mf(degrees)  = "+(Mf*180/pi));
+    disp("ωt/ωn= "+WtWn);
+    disp("ωt = "+Wt);
 end
